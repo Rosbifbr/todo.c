7352747495b538b6d8f37118ab6ba263c493a8da
Rodrigo Guimarães Ourique <rodrigo.guima.ourique@gmail.com>
2024-09-26 01:38:43 -0300

    not building. continue tomorrow

--- main.c
+++ main.c
@@ -27,13 +27,32 @@ enum http_methods {
   CONNECT
 };
 
+typedef struct {
+  char name[256];
+  int id;
+} todo;
+
+// stack alllocation for arra
+todo *todos[64];
+
 // MEDIUM LEVEL
+void generate_todos_html(char *buf) {
+  char *template = "<div id='todo%d' class='todo'>%s</div>\n";
+
+  for (int i = 0; i < sizeof(todos) / sizeof(todos[0]); i++) {
+    if (todos[i] == NULL)
+      continue;
+    sprintf(buf, template, todos[i]->id, todos[i]->name);
+  }
+  // free(template);
+}
+
 void write_response_headers(uint status, uint content_length, int fd) {
   // TODO: map status code
   char answer_headers[512];
   char *template = "HTTP/1.1 %d OK\r\n"
                    "Content-Length: %d\r\n"
-                   "Content-Type: text/plain\r\n"
+                   "Content-Type: text/html\r\n"
                    "Server: Ródŕîgô'ŝ himself\r\n\r\n";
 
   sprintf(answer_headers, template, status, content_length);
@@ -42,8 +61,36 @@ void write_response_headers(uint status, uint content_length, int fd) {
 
 // HIGH LEVEL - ROUTES
 void get_root_page(int req_file_desc) {
-  write_response_headers(404, 0, req_file_desc);
-  printf("Root called\n");
+  // after writing headers, write page
+  char *template =
+      "<html>\n"
+      "<meta>\n"
+      "<title>This is a todo app</title>\n"
+      "</meta>\n"
+      "<body>\n"
+      "<h1>This is a todo app</h1>\n"
+      "<div name='todos'>"
+      "%s" // TODO: todos go here
+      "</div>"
+      "<form action=\"/create\" method=\"POST\">\n"
+      "  <input type=\"text\" name=\"todo\" placeholder=\"Enter your todo\">\n"
+      "  <input type=\"submit\" value=\"Create\">\n"
+      "</form>\n"
+      "</html>\n";
+  // Generate todos for tempalteing
+  char *todos_string = malloc(REQUEST_BUFFER_SIZE);
+  generate_todos_html(todos_string);
+
+  // template the todos into res
+  char *res = malloc(strlen(template));
+  sprintf(res, template, todos_string);
+  free(todos_string);
+
+  // send it all to clietn
+  write_response_headers(200, strlen(res), req_file_desc);
+  write(req_file_desc, res, strlen(res));
+  free(res);
+  printf("root called!\n");
 }
 void post_create_todo(int req_file_desc) {
   write_response_headers(404, 0, req_file_desc);
@@ -61,10 +108,14 @@ void process_request(int req_file_desc) {
 
   // Map our request to other functions
   enum http_methods method;
-  if (buf[0] == 'G' && buf[1] == 'E' && buf[2] == 'T') {
+  if (strncmp(buf, "GET", 3) == 0) {
     method = GET;
-  } else {
+  } else if (strncmp(buf, "POST", 4) == 0) {
     method = POST;
+  } else {
+    write_response_headers(404, 0, req_file_desc);
+    close(req_file_desc);
+    return;
   }
 
   // getting the path
@@ -72,7 +123,6 @@ void process_request(int req_file_desc) {
   int path_start = -1;
   int path_end = -1;
   for (int i = 0; i < REQUEST_BUFFER_SIZE; i++) {
-    printf("a");
     if (path_start == -1 && buf[i] == '/') {
       path_start = i;
     } else if (path_start != -1 && buf[i] == ' ') {
@@ -84,6 +134,7 @@ void process_request(int req_file_desc) {
     }
   }
   memcpy(path, buf + path_start, path_end - path_start);
+  path[path_end - path_start] = '\0'; // Add null terminator to end of path
 
   if (method == GET && 0 == strcmp("/", path))
     get_root_page(req_file_desc);
@@ -103,7 +154,7 @@ void process_request(int req_file_desc) {
 }
 
 void handle_sigint(int fd) {
-  close(fd);
+  shutdown(fd, SHUT_RDWR);
   exit(0);
 }
 
@@ -142,6 +193,8 @@ int create_listen_socket(char *ip, int port) {
 }
 
 int main() {
+  todos[0]->id = 1;
+  strcpy(todos[0]->name, "This is a testing");
   // init
   int sock_desc = create_listen_socket(IP, PORT);
 
